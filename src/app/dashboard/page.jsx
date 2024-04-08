969f213676808cfe029a419e746750dcee917e5f
TinaTech-Developers <tinashephiri0@gmail.com>
2024-04-08 15:59:32 +0200

    added login

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,10 @@
+import LoginForm from "../components/LoginForm";
 import React from "react";
 
 function Login() {
   return (
     <div>
-      {/* <LoginForm /> */}
-      <h1 className="text-center text-white text-2xl">Login</h1>
+      <LoginForm />
     </div>
   );
 }
